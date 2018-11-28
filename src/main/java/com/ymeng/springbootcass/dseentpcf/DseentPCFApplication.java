4cce602211dc7c6c5220a01984282b381218f14c
yabinmeng <yabinmeng@gmail.com>
2018-11-28 09:29:47 -0800

    Update DseentPCFApplication.java

--- src/main/java/com/ymeng/springbootcass/dseentpcf/DseentPCFApplication.java
+++ src/main/java/com/ymeng/springbootcass/dseentpcf/DseentPCFApplication.java
@@ -3,9 +3,6 @@ package com.ymeng.springbootcass.dseentpcf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import java.util.UUID;
-
 @SpringBootApplication
 public class DseentPCFApplication {
 
